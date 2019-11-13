f43e8558f91a3b3f266f20bac31846c9f5d134bc
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-13 03:41:37 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,22 @@
 class Owner
+  attr_reader :name, :species
+  @@owners = []
+  def initialize(name)
+    @name = name
+    @species = human
+    @@owners << self
+  end
+  
+  def says_species
+    puts "I am a #{@species}."
+  end
+  
+  def self.all 
+    @@owners
+  end
+  
+  def self.count
+    @@owners.length 
+  end
   # code goes here
 end
