6f8db70a79d5ccac783be5365262158dbdb8e942
Nethelbert <gilblasse@gmail.com>
2019-11-15 22:56:33 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,14 @@
 class Cat
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name, :owner
+  
+  def initialize(name,owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+  end
+  
+  def owner=(new_owner)
+    @owner = new_owner
+  end
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name, :owner
+  @@all = []
+  
+  def initialize(name,owner)
+    @name = name
+    @owner = owner
+    @mood = nervous
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def owner=(new_owner)
+    @owner = new_owner
+  end
 end
