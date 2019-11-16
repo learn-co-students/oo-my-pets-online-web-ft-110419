ca1d73df821c69b40ef0390ded300ab3adb3674f
Nethelbert <gilblasse@gmail.com>
2019-11-16 01:01:47 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -15,19 +15,38 @@ class Owner
   
   def buy_cat(cat_name)
     # binding.pry
-    cat = Cat.all.find {|cat| cat.name == cat_name}
-    cat.owner = self 
+    # cat = Cat.all.find {|cat| cat.name == cat_name}
+    # cat.owner = self 
+    cat = Cat.new(cat_name,self)
+  end
+  
+  def buy_dog(dog_name)
+    dog = Dog.new(dog_name,self)
   end
   
   def cats 
-    binding.pry
     Cat.all.select {|cat| cat.owner == self}
   end
   
   def dogs 
     Dog.all.select {|dog| dog.owner == self}
   end
+  
+  def walk_dogs
+    self.dogs.map {|dog| dog.mood = "happy"}
+  end
+  
+  def feed_cats
+    self.cats.map {|cat cat.mood = "happy"}
+  end
 
+  def sell_pets
+    self.cats.map do |cat|
+      cat.mood = "happy" 
+      cat.owner = nil
+    end
+  end
+  
   def self.all
     @@all
   end
