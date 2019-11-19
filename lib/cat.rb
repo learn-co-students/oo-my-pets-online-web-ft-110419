5037ced76c0fa1e1d91e4a9a27e79100539f2bd1
Kenneth Andrews <kennethandrews@protonmail.com>
2019-11-19 02:40:49 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,23 @@
 class Cat
-  # code goes here
+
+  @@all = []
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  def initialize(name, owner)
+    @name = name
+    self.owner = owner 
+    self.mood = 'nervous'
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end 
+
+  def buy_cat
+    
+  end
+
 end
