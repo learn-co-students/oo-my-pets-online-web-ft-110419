747a14b300db9683a27045d02b71d5508442ecd4
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-11-20 10:41:13 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -41,7 +41,19 @@ class Owner
   end
 
   def sell_pets
-  
+    self.dogs.each do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    end
+
+    self.cats.each do |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{self.dogs.length} dog(s), and #{self.cats.length} cat(s)."
   end
 
   def self.all
