c9423821919cede620a7f1a0932b18790bbe7374
Ivana <ivana_pile@yahoo.com>
2019-11-21 18:29:53 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,22 @@
 class Cat
-  # code goes here
+attr_accessor :owner, :mood #can change its owner   #can change its mood
+attr_reader :name #can't change its name
+ @@all = []
+  
+ def initialize (name, owner)#initializes with a name and an Owner
+  @name = name
+  @owner = owner
+  @mood =  "nervous" 
+  #initializes with a nervous mood
+  @@all << self
+end  
+
+#Class methods
+#knows all the cats
+  
+def self.all
+  @@all
+end
+
+    
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
+  attr_accessor :owner, :mood
+  attr_reader :name
+@@all = []
+  def initialize (name, owner)
+  @name = name
+  @owner = owner
+  @mood = "nervous"
+  @@all << self
   # code goes here
 end
+def mood
+  @mood 
+end
+
+
+
+def self.all
+  @@all.include?(@dog)
+  @@all
+end
+
+
+end
+
