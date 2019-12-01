c9382ac809bae847720821290128ff8581f74be3
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-01 04:23:27 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,25 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    save
+  end
+
+  def self.mood
+    @mood
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
 end
